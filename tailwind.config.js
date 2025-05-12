2015d6ef025c89b3a05e2dae6ac8c03fc89f2080
damontana-web <david.montana@healthscope.com.au>
2025-05-12 11:55:45 +1000

    UI colour palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,9 @@ export default {
     extend: {
       colors: {
         brand: { 
-          500: '#2A3A8F',  // Your primary brand color
-          400: '#28ABE3',  // Your secondary brand color
-          600: '#1E2A6B',  // Darker shade of primary for hover states
+          500: '#1B91F2',  // Updated primary brand color
+          400: '#1B91F2',  // Your secondary brand color
+          600: '#2A3A8F',  // Darker shade of primary for hover states
         },
         navy: { 500: '#1e3a8a' },
       },
